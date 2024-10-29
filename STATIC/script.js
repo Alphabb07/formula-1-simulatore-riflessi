9f7e373bb7305e23babf57ced8d252ed6c1ece52
Alphabb07 <162568674+Alphabb07@users.noreply.github.com>
2024-10-29 18:28:03 +0100

    Aggiornamento 2.0
    Codice ancora non funzionante, iniziata la parte del javascript.

--- STATIC/script.js
+++ STATIC/script.js
@@ -1,1 +1,24 @@
-console.log("Javascript e' stato caricato correttamente")
+console.log("Javascript è stato caricato correttamente")
+
+let countdown = 3;
+let timer = dcument.querySelector("#timer")
+let GaS = document.querySelector("#GAS")
+let inizio;
+
+function conteggio(){
+if (countdown  > 0){
+    timer.textContent = countdown;
+    countdown = countdown - 1;
+    setTimeout(conteggio, 1000);
+
+
+
+} else {
+timer.textContent = "VIA!";
+inizio = new Date().getTime();
+
+    }
+}
+
+
+conteggio();    
