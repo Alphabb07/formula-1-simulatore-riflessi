81a696db52b175af5745452a5dff77f6dcf563fc
Alphabb07 <162568674+Alphabb07@users.noreply.github.com>
2024-10-30 16:45:52 +0100

    Progetto Finito
    Il pulsante una volta cliccato è in grado di misurare il tempo di reazione una volta cliccato e stamparlo a schermo.

--- STATIC/script.js
+++ STATIC/script.js
@@ -1,24 +1,47 @@
 console.log("Javascript è stato caricato correttamente")
 
 let countdown = 3;
-let timer = dcument.querySelector("#timer")
-let GaS = document.querySelector("#GAS")
+let timer = document.querySelector("#timer")
+let GAS = document.querySelector("#GAS")
 let inizio;
+let fine;
+let risultato;
+let reflex;
 
 function conteggio(){
 if (countdown  > 0){
     timer.textContent = countdown;
     countdown = countdown - 1;
     setTimeout(conteggio, 1000);
+    GAS.disabled = true;
 
 
 
 } else {
 timer.textContent = "VIA!";
 inizio = new Date().getTime();
+GAS.disabled = false;
 
-    }
+    
 }
+}
+
+GAS.addEventListener("click", function() {
+if (inizio) {
+fine = new Date().getTime();
+reflex = fine - inizio;
+risultato = ( reflex + " ms");
+timer.textContent = "";
+inizio = null;
+console.log("Risultato: " + risultato);
+timer.textContent = ("Il tuo risultato e': " +risultato);
+
+
+
+}
+
+
+})
 
 
 conteggio();    
